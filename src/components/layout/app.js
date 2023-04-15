9aab5610fe8254c785260cb443f9440ceee07efa
Mark-david <ojukwudavid59@outlook.com>
2023-04-15 16:36:27 +0100

    gmail-clone

--- src/components/layout/app.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector('#menu');
-
-const sidebar = document.querySelector('#side_bar');
-
-menu.addEventListener('click', () => {
-  sidebar.classList.toggle('show_sidebar');
-});
